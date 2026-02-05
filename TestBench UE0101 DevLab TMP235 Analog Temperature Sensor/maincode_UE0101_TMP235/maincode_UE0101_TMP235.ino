a404944a73df99622d3134eba2ed6da877c5def8
emilianomolinav <emiliano.molina@uelectronics.com>
2026-02-05 12:11:08 -0600

    Codigo funcional final

--- TestBench UE0101 DevLab TMP235 Analog Temperature Sensor/maincode_UE0101_TMP235/maincode_UE0101_TMP235.ino
+++ TestBench UE0101 DevLab TMP235 Analog Temperature Sensor/maincode_UE0101_TMP235/maincode_UE0101_TMP235.ino
@@ -6,6 +6,7 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoPixel.h>
+#include <ArduinoJson.h>
 #include <Arduino.h>
 
 // ---- Pines I2C de OLED ----
@@ -15,7 +16,7 @@
 // ---- Pines de lectura para sensor analógico [6]----
 #define SENS_1 0
 #define SENS_2 1
-#define SENS_3 8
+#define SENS_3 2
 #define SENS_4 4
 #define SENS_5 5
 #define SENS_6 6
@@ -25,19 +26,20 @@
 #define RELAY 21
 #define NEOPIX 8
 
-
 // ---- Configuración de la pantala OLED ----
 #define OLED_RESET -1                                                      // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_WIDTH 128                                                   // OLED display width, in pixels
 #define SCREEN_HEIGHT 64                                                   // OLED display height, in pixels
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  // Objeto de la OLED
 
-Adafruit_NeoPixel np = Adafruit_NeoPixel(11, NEOPIX, NEO_GRB + NEO_KHZ800);  // Objeto NeoPixel
+Adafruit_NeoPixel np = Adafruit_NeoPixel(3, NEOPIX, NEO_GRB + NEO_KHZ800);  // Objeto NeoPixel
 
 // ---- Banderas y Variables ----
 bool flagSW = false;
 int animX = 0;
 
+// ==== Declaración de Variables JSON ====
+StaticJsonDocument<128> sendJSON;
 
 void setup() {
 
@@ -62,7 +64,7 @@ void setup() {
   analogSetAttenuation(ADC_11db);
 
   // ---- Inicialización del Neopixel ----
-  np.begin();  // Inicialización de objeto NeoPixel
+  np.begin();
   np.setBrightness(20);
   np.setPixelColor(0, np.Color(0, 255, 0));
   np.show();
@@ -70,52 +72,65 @@ void setup() {
 
 void loop() {
 
+  sendJSON.clear();
+
   // ---- HEADER ---
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(25, 0);
   display.println("TEMPERATURA");
-  // ---- Línea animada ----
-  display.drawLine(0, 12, animX, 12, SSD1306_WHITE);
-
-  animX += 4;  // velocidad
+  display.drawLine(0, 12, animX, 12, SSD1306_WHITE);  // Línea animada
+  animX += 4;                                         // velocidad
   if (animX > 127) animX = 0;
 
 
-  // ---- Valores medidos ----
-  int t1 = readTempC(SENS_1);
-  int t2 = readTempC(SENS_2);
-  int t3 = readTempC(SENS_3);
-  int t4 = readTempC(SENS_4);
-  int t5 = readTempC(SENS_5);
-  int t6 = readTempC(SENS_6);
+  // ---- Valores de Temperatura y muestreo ----
+  float t1 = readTempC(SENS_1);
+  float t2 = readTempC(SENS_2);
+  float t3 = readTempC(SENS_3);
+  float t4 = readTempC(SENS_4);
+  float t5 = readTempC(SENS_5);
+  float t6 = readTempC(SENS_6);
 
   display.setCursor(5, 20);
   display.print("S1: ");
-  display.print(t1);
+  display.print(t1, 0);
   display.println(" C");
+
   display.setCursor(5, 35);
   display.print("S2: ");
-  display.print(t2);
+  display.print(t2, 0);
   display.println(" C");
+
   display.setCursor(5, 50);
   display.print("S3: ");
-  display.print(t3);
+  display.print(t3, 0);
   display.println(" C");
 
   display.setCursor(70, 20);
   display.print("S4: ");
-  display.print(t4);
+  display.print(t4, 0);
   display.println(" C");
+
   display.setCursor(70, 35);
   display.print("S5: ");
-  display.print(t5);
+  display.print(t5, 0);
   display.println(" C");
+
   display.setCursor(70, 50);
   display.print("S6: ");
-  display.print(t6);
+  display.print(t6, 0);
   display.println(" C");
+  sendJSON["s6"] = t6;
+
+  sendJSON["s1"] = String(t1, 2);
+  sendJSON["s2"] = String(t2, 2);
+  sendJSON["s3"] = String(t3, 2);
+
+  sendJSON["s4"] = String(t4, 2);
+  sendJSON["s5"] = String(t5, 2);
+  sendJSON["s6"] = String(t6, 2);
 
 
   // ---- Botón de Switch ---
@@ -123,7 +138,6 @@ void loop() {
     delay(30);  // debounce
     if (digitalRead(SWITCH) == LOW) {
       flagSW = !flagSW;
-      Serial.println("Presionado");
       while (digitalRead(SWITCH) == LOW)
         ;  // espera a soltar
     }
@@ -134,9 +148,11 @@ void loop() {
   if (flagSW == true) {
     digitalWrite(RELAY, LOW);
     display.println("ON");
+    sendJSON["relay"] = "1";
   } else {
     digitalWrite(RELAY, HIGH);
     display.println("OFF");
+    sendJSON["relay"] = "2";
   }
 
   display.display();
@@ -149,8 +165,9 @@ void loop() {
     np.show();
   }
 
-
-  delay(50);
+  serializeJson(sendJSON, Serial);
+  Serial.println();
+  delay(100);
 }
 
 
