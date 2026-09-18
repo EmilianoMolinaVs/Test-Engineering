06005e56353e5eb99424cabddf633a5820a68615
Emiliano Molina <emilianomolinavaldes@gmail.com>
2026-09-18 09:58:34 -0600

    feat: final firmware test attiny85 pc0702 RS485+

---
+++ TestBench ENERSI/ENERSI_RS485_AtTiny_PC0702/ENERSI_RS485_AtTiny_PC0702.ino
@@ -0,0 +1,43 @@
+#include <SoftwareSerial.h>
+
+#define RX_PIN 3
+#define TX_PIN 4
+#define BLINK_PIN 1  // >> BLINK en GPIO PB1
+
+SoftwareSerial miSerial(RX_PIN, TX_PIN);  // RX, TX
+
+// Variables para millis()
+unsigned long previousMillis = 0;
+const long interval = 500;
+bool ledState = HIGH;
+
+void setup() {
+  miSerial.begin(9600);
+  miSerial.println("Hola Mundo");
+
+  pinMode(BLINK_PIN, OUTPUT);
+  digitalWrite(BLINK_PIN, HIGH);
+}
+
+void loop() {
+  // 1. Escucha Serial No Bloqueante
+  if (miSerial.available() > 0) {
+    String input = miSerial.readStringUntil('\n');
+    input.trim(); 
+    
+    if (input == "ping") {
+      miSerial.println("pong");
+    } else {
+      miSerial.println("FAIL: invalid option");
+    }
+  } 
+  
+  // 2. Parpadeo No Bloqueante
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    ledState = !ledState;
+    digitalWrite(BLINK_PIN, ledState);
+  }
+}
